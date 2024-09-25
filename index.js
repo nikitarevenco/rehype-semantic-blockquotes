40c772154af0068a410989b9714ffb4f6ff39d24
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-09-25 02:59:34 +0100

    feat: support for mdx

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+import { fromMarkdown } from "mdast-util-from-markdown";
+import { mdxjs } from "micromark-extension-mdxjs";
+import { mdxFromMarkdown, mdxToMarkdown } from "mdast-util-mdx";
 import { toMarkdown } from "mdast-util-to-markdown";
 import { visit } from "unist-util-visit";
 
@@ -29,7 +32,7 @@ const checkLastChild = (caption) => {
 /**
  * @typedef {import('./File1.js').MyObject1} MyObject1
  */
-const parse = (blockquote, opts) => {
+const parse = (blockquote, opts, isMdx) => {
   // would not make sense to transform if there is only 1 child
   if (blockquote.children.length >= 2) {
     const lastChild = blockquote.children.at(-1);
@@ -43,15 +46,42 @@ const parse = (blockquote, opts) => {
 
     // toMarkdown does not support arrays, so we havo use this approach
     // Also, toMarkdown adds a newline at the end of its output, which we remove with slice
-    const [contentMd, captionMd] = [content, caption].map((ast) =>
-      toMarkdown({ type: "root", children: ast }).slice(0, -1),
-    );
-    const html = wrapWithHtml(contentMd, captionMd, opts);
 
-    // replace markdown syntax with the modified HTML
-    blockquote.type = "html";
-    blockquote.children = undefined;
-    blockquote.value = html;
+    if (isMdx) {
+      const contentMd = toMarkdown(
+        { type: "root", children: content },
+        { extensions: [mdxToMarkdown()] },
+      );
+      const captionMd = toMarkdown(
+        { type: "root", children: caption },
+        { extensions: [mdxToMarkdown()] },
+      );
+      const html = wrapWithHtml(contentMd, captionMd, opts);
+
+      /**
+       * Syntax tree with MDX blockquote
+       */
+      const astHtmlMdx = fromMarkdown(html, {
+        extensions: [mdxjs()],
+        mdastExtensions: [mdxFromMarkdown()],
+      });
+      const mdxEl = astHtmlMdx.children[0];
+
+      blockquote.type = mdxEl.type;
+      blockquote.name = mdxEl.name;
+      blockquote.attributes = mdxEl.attributes;
+      blockquote.children = mdxEl.children;
+    } else {
+      const [contentMd, captionMd] = [content, caption].map((ast) =>
+        toMarkdown({ type: "root", children: ast }).slice(0, -1),
+      );
+      const html = wrapWithHtml(contentMd, captionMd, opts);
+
+      // replace markdown syntax with the modified HTML
+      blockquote.type = "html";
+      blockquote.children = undefined;
+      blockquote.value = html;
+    }
   }
 };
 
@@ -68,9 +98,15 @@ const remarkSemanticBlockquote = (
 ) => {
   return (tree) => {
     /**
-     * @param {string} blockquote
+     * @param {strieeng} blockquote
      */
-    visit(tree, "blockquote", (blockquote) => parse(blockquote, opts));
+    const isMdx =
+      tree.children &&
+      tree.children[0] &&
+      tree.children[0].type &&
+      tree.children[0].type === "mdxjsEsm";
+
+    visit(tree, "blockquote", (blockquote) => parse(blockquote, opts, isMdx));
   };
 };
 
