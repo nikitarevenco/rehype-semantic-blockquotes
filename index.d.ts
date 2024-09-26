f5606ef05f9a064d9de10d9c1548db9c453380de
Nikita Revenco <154856872+nikitarevenco@users.noreply.github.com>
2024-09-26 20:26:08 +0100

    feat: add generated .d.ts files

--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,18 @@
 /**
  * A rehype plugin to extend blockquote syntax to make it simple to mention/cite sources in a semantically correct way.
- *
  * More info: [https://github.com/nikitarevenco/remark-semantic-blockquotes](https://github.com/nikitarevenco/remark-semantic-blockquotes)
+ *
+ * @param {Object} opts Configuration for the plugin
+ * @param {string} opts.figure Attribute name for the `<figure>` element
+ * @param {string} opts.blockquote Attribute name for the `<blockquote>` element
+ * @param {string} opts.figcaption Attribute name for the `<figcaption>` element
+ * @param {string} opts.syntax Syntax to use to trigger the plugin's effect
+ * @returns {import("hast").Root} The transformed HAST root node
  */
-export default function rehypeSemanticBlockquotes(options: {
-  /**
-   * Attribute name for the <figure> element, default: "data-blockquote-container"
-   */
-  figure?: string;
-  /**
-   * Attribute name for the <blockquote> element, default: "data-blockquote-content"
-   */
-  blockquote?: string;
-  /**
-   * Attribute name for the <figcaption> element, default: "data-blockquote-credit"
-   */
-  figcaption?: string;
-  /**
-   * Syntax to use to have the plugin's effect take place, default: "@ "
-   */
-  syntax?: string;
-}): (tree: Root) => undefined;
-
-export type Root = import("hast").Root;
+export default function rehypeSemanticBlockquotes(opts?: {
+    figure: string;
+    blockquote: string;
+    figcaption: string;
+    syntax: string;
+}): import("hast").Root;
+//# sourceMappingURL=index.d.ts.map
